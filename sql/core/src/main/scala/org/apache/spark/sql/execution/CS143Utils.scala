f1c547ce3b55c7977b3ace41033d4c29f90b8597
Abdullah Albanyan <abalbanyan@hotmail.com>
2017-06-09 12:17:36 -0700

    bug fix maybe

--- sql/core/src/main/scala/org/apache/spark/sql/execution/CS143Utils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/CS143Utils.scala
@@ -1,6 +1,6 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file dis.tributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
@@ -242,7 +242,7 @@ object CachingIteratorGenerator {
           val key_proj = cacheKeyProjection(row)
 
           if (!cache.containsKey(key_proj)) {
-            cache.put(key_proj, Row(udfProject(row)))
+            cache.put(key_proj, udfProject(row))
           }
           Row.fromSeq(pre_proj ++ cache.get(key_proj) ++ post_proj)
         } else {
